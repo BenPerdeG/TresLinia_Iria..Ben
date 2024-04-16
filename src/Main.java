8aa473d27debc26876240fd9e5bb4cc323831262
BenPerdeG <bperez@elpuig.xeill.net>
2024-04-16 17:41:02 +0200

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido al increible y turbo dinámico 'Tres en Raya Hipersport 2024'");
+        System.out.println("Por favor seleccione que desea hacer:");
+        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir");
     }
 }
