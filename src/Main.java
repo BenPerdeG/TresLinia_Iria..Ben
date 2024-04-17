9ad6ba32836d09a12339e268ecff3aa4e7ff46fb
BenPerdeG <bperez@elpuig.xeill.net>
2024-04-17 19:20:01 +0200

    Loop y correccion final

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,33 @@ public class Main {
 
         System.out.println("Bienvenido al increíble y turbo dinámico 'Tres en Raya Hipersport 2024'");
 
-        System.out.println("Por favor seleccione que desea hacer:");
-        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir\n");
+            while(true) {
+            System.out.println("Por favor seleccione que desea hacer:");
+            System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir\n");
 
-        try{
-            int input= sc.nextInt();
 
-            switch (input){
-                case 1:
-                    //Nueva partida
-                    System.out.println("Su selección ha sido: Nueva partida");
-                    break;
-                case 2:
-                    //Cargar partida
-                    System.out.println("Su selección ha sido: Cargar partida");
-                    break;
-                case 3:
-                    //Configuración
-                    System.out.println("Su selección ha sido: Configuración");
-                    break;
-                case 4:
-                    //Finalizar
-                    System.out.println("¡Hasta pronto!");
-                    break;
-                default:
-                    System.out.println("Ha habido un error con el input. Prueba de nuevo");
+                String input = sc.nextLine();
+                switch (input) {
+                    case "1":
+                        //Nueva partida
+                        System.out.println("Su selección ha sido: Nueva partida");
+                        break;
+                    case "2":
+                        //Cargar partida
+                        System.out.println("Su selección ha sido: Cargar partida");
+                        break;
+                    case "3":
+                        //Configuración
+                        System.out.println("Su selección ha sido: Configuración");
+                        break;
+                    case "4":
+                        //Finalizar
+                        System.out.println("¡Hasta pronto!");
+                        break;
+                    default:
+                        System.out.println("Ha habido un error con el input. Prueba de nuevo");
+                        break;
+                }
             }
-        }catch (Exception e){
-            System.out.println("Ha habido un error con el input. Prueba de nuevo"); // Si existe cualquier otro error, dentro del try
-        }
     }
 }
