6c49d7bfc623a58db6fcda3cf061a3a6cea6966b
Iria141 <ircabla@elig.xeill.net>
2024-04-17 18:44:12 +0200

    eliminación de métodos

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        bienvenida();
-        menu();
+        System.out.println("Bienvenido al increíble y turbo dinámico 'Tres en Raya Hipersport 2024'");
+
+        System.out.println("Por favor seleccione que desea hacer:");
+        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir");
+
 
         try{
+
             int input= sc.nextInt();
             if (input>=5){
                 System.out.println("Ha habido un error con el input. Prueba de nuevo");
-                menu();
+
             }
         }catch (Exception e){
             System.out.println("Ha habido un error con el input. Prueba de nuevo");
-                menu();
+
         }
-    }
-
-
-//Métodos
-
-    public static void bienvenida (){
-        System.out.println("Bienvenido al increíble y turbo dinámico 'Tres en Raya Hipersport 2024'");
-
-    }
-
-    public static void menu(){
-        System.out.println("Por favor seleccione que desea hacer:");
-        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir");
     }
 }
