c795b8cfdb18c7d862dbd5ce70b98f8bf59851b5
Iria141 <166645198+Iria141@users.noreply.github.com>
2024-04-17 15:09:27 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 
-public class Main {
+public class Tresenraya {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bienvenido al increible y turbo dinámico 'Tres en Raya Hipersport 2024'");
-        System.out.println("Por favor seleccione que desea hacer:");
-        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir");
+        bienvenida();
+        menu();
 
         try{
             int input= sc.nextInt();
             if (input>=5){
                 System.out.println("Ha habido un error con el input. Prueba de nuevo");
-                //metodo menu
+                menu();
             }
         }catch (Exception e){
             System.out.println("Ha habido un error con el input. Prueba de nuevo");
-            //metodo menu
+                menu();
         }
+    }
+
+
+//Métodos
+
+    public static void bienvenida (){
+        System.out.println("Bienvenido al increíble y turbo dinámico 'Tres en Raya Hipersport 2024'");
+
+    }
+
+    public static void menu(){
+        System.out.println("Por favor seleccione que desea hacer:");
+        System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir");
     }
 }
