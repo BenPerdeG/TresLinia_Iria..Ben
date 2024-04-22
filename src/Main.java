682e9c93539b2ccf831d933d049db7324e7181f0
BenPerdeG <bperez@elpuig.xeill.net>
2024-04-22 16:06:38 +0200

    Cambio menor de texto

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         TUI p = new TUI();
         System.out.println("Bienvenido al increíble y turbo dinámico 'Tres en Raya Hipersport 2024'");
+        System.out.println("-----------------------------------------------------------------------");
         while (true) {
             System.out.println("Por favor seleccione que desea hacer:");
             System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir\n");
             p.menu();
-            }
         }
+    }
 }
 
