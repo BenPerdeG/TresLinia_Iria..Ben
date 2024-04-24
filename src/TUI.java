f418092d2a677954e004c5dcf201f3026d91c95c
Iria141 <ircabla@elig.xeill.net>
2024-04-24 19:11:01 +0200

    nueva clase joc

--- src/TUI.java
+++ src/TUI.java
@@ -24,35 +24,34 @@ public class TUI {
         }
     }
 
-    public static void menu(){
+    public static void menu() {
         bucle:
         while (true) {
             System.out.println("Por favor seleccione que desea hacer:");
             System.out.println(" 1.Nueva Partida \n 2.Cargar Partida \n 3.Configuración\n 4.Salir\n");
-        switch (seleccion()){
-            case 1:
-                //Nueva partida
-                System.out.println("Su selección ha sido: Nueva partida");
-                Main.nueva_partida();
-                break;
-            case 2:
-                //Cargar partida
-                System.out.println("Su selección ha sido: Cargar partida");
-                Main.cargar_partida();
-                break;
-            case 3:
-                //Configuración
-                System.out.println("Su selección ha sido: Configuración");
-                Main.congiguracion();
-                break;
-            case 4:
-                //Finalizar
-                System.out.println("¡Hasta pronto!\n");
-                break bucle;
-            default:
-                System.out.println("Ha habido un error con el input. Prueba de nuevo\n");
+            switch (seleccion()) {
+                case 1:
+                    //Nueva partida
+                    System.out.println("Su selección ha sido: Nueva partida");
+                    Main.nueva_partida();
+                    break;
+                case 2:
+                    //Cargar partida
+                    System.out.println("Su selección ha sido: Cargar partida");
+                    Main.cargar_partida();
+                    break;
+                case 3:
+                    //Configuración
+                    System.out.println("Su selección ha sido: Configuración");
+                    Main.congiguracion();
+                    break;
+                case 4:
+                    //Finalizar
+                    System.out.println("¡Hasta pronto!\n");
+                    break bucle;
+                default:
+                    System.out.println("Ha habido un error con el input. Prueba de nuevo\n");
+            }
         }
     }
-}
-
 }
