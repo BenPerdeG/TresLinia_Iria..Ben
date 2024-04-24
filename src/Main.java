fe2017250cb774bd68dc605620aff27ecbbb25e7
Iria141 <ircabla@elig.xeill.net>
2024-04-24 20:05:15 +0200

    Nueva partida, inicializada. Muestra tablero vacio y establece fichas.
    Existen errores para corregir

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,25 @@ public class Main {
     //Metodos
     public static void nueva_partida (){
         Joc.mostrar_tablero();
+        // Comenzar con el tablero vacio
+        int i;
+        int j;
+
+        for ( i =0 ; i<5; i++){
+            for (j= 0; j<5; j++){
+                tablero[i][j]= ' ';
+            }
+        }
+        //Definir con que carácter juega cada uno de los jugadores y quien comienza.
+
+        System.out.println("La máquina juega con: 'x'");
+        System.out.println("Usted juega con; 'o'");
     }
+
     public static void cargar_partida (){
         not_implemented();
     }
+
     public static void congiguracion (){
         not_implemented();
     }
