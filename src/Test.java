3a37f69ad5b48b4a720ee9b82d7cc35c73123f34
BenPerdeG <bperez@elpuig.xeill.net>
2024-04-26 17:36:03 +0200

    Arreglado tablero'

--- src/Test.java
+++ src/Test.java
@@ -4,22 +4,18 @@ public class Test {
     // Comprobar tablero en blanco
     @org.junit.jupiter.api.Test
     void Tablero_Blanco() {
-        Joc j = new Joc();
-        String linea1 = " | | ";
-        String linea2 = " | | ";
-        String linea3 = " | | ";
-        String lineamid = "_____";
+            Joc j = new Joc();
+            String linea1 = " | | ";
+            String linea2 = " | | ";
+            String linea3 = " | | ";
 
 
-        char tablero[][] = new char[5][5];
+            char tablero[][] = new char[5][3];
 
-        tablero[0] = linea1.toCharArray();
-        tablero[1] = lineamid.toCharArray();
-        tablero[2] = linea2.toCharArray();
-        tablero[3] = lineamid.toCharArray();
-        tablero[4] = linea3.toCharArray();
-        Assertions.assertArrayEquals(tablero, j.tablero());
-
+            tablero[0] = linea1.toCharArray();
+            tablero[1] = linea2.toCharArray();
+            tablero[2] = linea3.toCharArray();
+            Assertions.assertArrayEquals(tablero,j.tablero());
     }
 
     @org.junit.jupiter.api.Test
