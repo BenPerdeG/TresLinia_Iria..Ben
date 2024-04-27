93495c0995d706c5d5e782506bfc092db9659b4b
Iria141 <ircabla@elpuig.xeill.net>
2024-04-28 00:04:09 +0200

    Test métodos de nueva partida (Simulador de coordenadas, jugada correcta)

--- src/Joc.java
+++ src/Joc.java
@@ -1,13 +1,10 @@
 public class Joc {
+    boolean turno_jugador1 = true; // Inicia con el jugador 1
+    char [][] tablero_actual = tablero();
 
-boolean turno_jugador1 = false;
-char [][] tablero_actual = tablero();
-
-
+//INICIALIZAR EL TABLERO EN BLANCO
     public static char[][]tablero(){
-    //Rellenar tablero (array 3,3) de espacios en blanco
         char tab [][] = new char[3][3];
-
         for (int i = 0; i<tab.length; i++){
             for (int j = 0; j<tab.length; j++){
                 tab[i][j] = ' ';
@@ -16,21 +13,23 @@ char [][] tablero_actual = tablero();
         return tab;
     }
 
+//MOSTRAR TABLERO ACTUALIZADO
     public static void mostrar_tablero(){
         System.out.println(tablero()[0][0]+"|"+tablero()[0][1]+"|"+tablero()[0][2]+"\n"
                 +tablero()[1][0]+"|"+tablero()[1][1]+"|"+tablero()[1][2]+"\n"
                 +tablero()[2][0]+"|"+tablero()[2][1]+"|"+tablero()[2][2]);
     }
 
-    public boolean get_jugador1 (){
-        //Alternar si es o no el turno de jugador 1
+    public boolean alternar_turno (){
+
         if(turno_jugador1){
-            turno_jugador1=false;
+            turno_jugador1=true;
         }else{
-            turno_jugador1 = true;
+            turno_jugador1 = false;
         }
         return turno_jugador1;
     }
+
     public void jugarX(int coord1, int coord2){
 
         tablero_actual[coord1-1][coord2-1]='x';
--- src/Test.java
+++ src/Test.java
@@ -1,31 +1,52 @@
 import org.junit.jupiter.api.Assertions;
-public class Test {
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
     // Comprobar tablero en blanco
     @org.junit.jupiter.api.Test
-    void Tablero_Blanco() {
+    void Tablero_Blanco() { //Joc
         char tab [][] = new char[3][3];
-
         for (int i = 0; i<tab.length; i++){
             for (int j = 0; j<tab.length; j++){
                 tab[i][j] = ' ';
             }
         }
-            Assertions.assertArrayEquals(tab,Joc.tablero());
+        Assertions.assertArrayEquals(tab,Joc.tablero());
     }
-
     @org.junit.jupiter.api.Test
-    void TurnoJugador() {
+    void TurnoJugador() { //Joc
         Joc j = new Joc();
-        Assertions.assertTrue(j.get_jugador1());
+        Assertions.assertTrue(j.alternar_turno());
     }
 
     @org.junit.jupiter.api.Test
-    void una_jugada() {
+    void una_jugada() { //Joc
         Joc j = new Joc();
         j.jugarX(1,1);
         Assertions.assertEquals('x',j.tablero_actual[0][0]);
         j.jugarO(2,2);
         Assertions.assertEquals('o',j.tablero_actual[0][0]);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void Coord_simulador() {
+        String data = "2\n2\n"; // Coordenada simulada (2 2), como si fuera el jugador 1.
+        InputStream input = new ByteArrayInputStream(data.getBytes());
+        System.setIn(input);
+    }
+
+    @org.junit.jupiter.api.Test
+    void jugada_correcta() { //TUI
+        Coord_simulador();
+        //comprobar fila
+        Assertions.assertFalse(TUI.jugada_inicial(0, 2));
+        //comprobar columna
+        Assertions.assertFalse(TUI.jugada_inicial(2, 0));
+        //Coordenada completa valida
+        Assertions.assertTrue(TUI.jugada_inicial(2, 2));
     }
 }
